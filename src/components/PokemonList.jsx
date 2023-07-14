5ee4714051a4bdc47694b9ec89decfcdbce8e724
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-07-14 15:33:39 +0800

    Create PokemonList.jsx

---
+++ src/components/PokemonList.jsx
@@ -0,0 +1,9 @@
+import axios from "axios";
+import { useState } from "react";
+
+const PokemonList = () => {
+  const [pokemons, setPokemons] = useState([]);
+  return <div>PokemonList</div>;
+};
+
+export default PokemonList;
