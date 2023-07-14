ad53ac6525b7f659a5ca84146eda4396facd4a41
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-07-14 17:26:44 +0800

    Update Header.jsx

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -2,7 +2,7 @@ import AddModal from "../components/AddModal";
 
 const Header = () => {
   return (
-    <div className="flex justify-between px-7 py-4 bg-orange-100 fixed w-full">
+    <div className="flex flex-col gap-6 sm:flex-row justify-between px-7 py-4 bg-orange-100 fixed w-full">
       <h1 className="font-bold text-4xl">Hunt A Pokemon!</h1>
       <AddModal />
     </div>
