9221fb0241240e5ddc3a1070079e7d692dc68e84
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-07-14 17:47:16 +0800

    Update PokemonContext.jsx

--- src/context/PokemonContext.jsx
+++ src/context/PokemonContext.jsx
@@ -1,10 +1,38 @@
 /* eslint-disable react/prop-types */
-import { createContext, useState } from "react";
+import axios from "axios";
+import { createContext, useState, useEffect } from "react";
 
 const PokemonContext = createContext();
 
 const PokemonProvider = ({ children }) => {
-  const value = {};
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://pokeapi.co/api/v2/pokemon?limit=100"
+        );
+        const pokemons = response.data.results;
+        const updatedPokemons = await Promise.all(
+          pokemons.map(async (pokemon) => {
+            const res = await axios.get(pokemon.url);
+            return {
+              name: pokemon.name,
+              imageUrl: res.data.sprites.front_default,
+            };
+          })
+        );
+        setPokemons(updatedPokemons);
+      } catch (error) {
+        console.error("Error fetching Pokemon data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const value = { pokemons, setPokemons };
 
   return (
     <PokemonContext.Provider value={value}>{children}</PokemonContext.Provider>
