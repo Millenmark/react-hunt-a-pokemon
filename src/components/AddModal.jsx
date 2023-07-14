5c86bd82d9eb698f94b31005905fbbcb8b0744e4
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-07-14 16:23:13 +0800

    Update AddModal.jsx

--- src/components/AddModal.jsx
+++ src/components/AddModal.jsx
@@ -2,6 +2,12 @@ import { useState } from "react";
 
 export default function Modal() {
   const [showModal, setShowModal] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setShowModal(false);
+  };
+
   return (
     <>
       <button
@@ -13,7 +19,10 @@ export default function Modal() {
       </button>
       {showModal ? (
         <>
-          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+          <form
+            onSubmit={handleSubmit}
+            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+          >
             <div className="relative w-auto my-6 mx-auto max-w-3xl">
               {/*content*/}
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
@@ -31,13 +40,17 @@ export default function Modal() {
                 </div>
                 {/*body*/}
                 <div className="relative p-6 flex-auto">
-                  <p className="my-4 text-slate-500 text-lg leading-relaxed">
-                    I always felt like I could do anything. That’s the main
-                    thing people are controlled by! Thoughts- their perception
-                    of themselves! Thre slowed down by their perception of
-                    themselves. If yore taught you can’t do anything, you won’t
-                    do anything. I was taught I could do everything.
-                  </p>
+                  {/* <p className="my-4 text-slate-500 text-lg leading-relaxed">
+                    
+                  </p> */}
+                  <input
+                    className="w-full p-3"
+                    type="text"
+                    name="name"
+                    id="name"
+                    placeholder="Enter the name of your pokemon"
+                    required
+                  />
                 </div>
                 {/*footer*/}
                 <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
@@ -50,15 +63,14 @@ export default function Modal() {
                   </button>
                   <button
                     className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => setShowModal(false)}
+                    type="submit"
                   >
                     Add This Pokemon
                   </button>
                 </div>
               </div>
             </div>
-          </div>
+          </form>
           <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
         </>
       ) : null}
